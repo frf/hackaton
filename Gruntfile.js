5e0bb2dc7bbaaf50e159eb895967271621aab4da
Hugo Medeiros <hugo.hugomedeiros@gmail.com>
2017-04-08 15:21:38 -0300

    added Automated Tasks – Grunt, Sass

---
+++ Gruntfile.js
@@ -0,0 +1,133 @@
+/* jshint node:true */
+module.exports = function( grunt ) {
+	'use strict';
+
+	require( 'load-grunt-tasks' )( grunt );
+
+	var odinConfig = {
+
+		// gets the package vars
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		// setting folder templates
+		dirs: {
+			css: '../assets/css',
+			js: 'src/assets/js',
+			js_public: 'src/assets/js',
+			sass: 'src/assets/sass',
+			images_public: 'src/assets/images',
+			fonts: 'src/assets/fonts',
+			core: '../core',
+			tmp: 'tmp'
+		},
+
+		// javascript linting with jshint
+		jshint: {
+			options: {
+				jshintrc: 'src/assets/js/.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'<%= dirs.js %>/main.js'
+			]
+		},
+
+		// uglify to concat and minify
+		uglify: {
+			dist: {
+				files: {
+					'<%= dirs.js_public %>/main.min.js': [
+						'<%= dirs.js %>/libs/*.js', // External libs/plugins
+						'<%= dirs.js %>/main.js'    // Custom JavaScript
+					]
+				}
+			}
+		},
+
+		// compile scss/sass files to CSS
+		compass: {
+			dist: {
+				options: {
+					config: 'config.rb',
+					outputStyle: 'compressed'
+				}
+			}
+		},
+
+		// watch for changes and trigger compass, jshint, uglify and livereload browser
+		watch: {
+			compass: {
+				files: [
+					'<%= dirs.sass %>/**'
+				],
+				tasks: ['compass']
+			},
+			js: {
+				files: [
+					'<%= jshint.all %>'
+				],
+				tasks: ['jshint', 'uglify']
+			},
+			livereload: {
+				options: {
+					livereload: true
+				},
+				files: [
+					'<%= dirs.css %>/*.css',
+					'<%= dirs.js_public %>/*.js',
+					'../**/*.html'
+				]
+			}
+		},
+
+		// image optimization
+		imagemin: {
+			dist: {
+				options: {
+					optimizationLevel: 7,
+					progressive: true
+				},
+				files: [{
+					expand: true,
+					cwd: '<%= dirs.images %>/',
+					src: '**/*.{png,jpg,gif}',
+					dest: '<%= dirs.images %>/'
+				}]
+			}
+		}
+
+	};
+
+	// Initialize Grunt Config
+	// --------------------------
+	grunt.initConfig( odinConfig );
+
+	// Register Tasks
+	// --------------------------
+
+	// Default Task
+	grunt.registerTask( 'default', [
+		'jshint',
+		'compass',
+		'uglify'
+	] );
+
+	// Optimize Images Task
+	grunt.registerTask( 'optimize', ['imagemin'] );
+
+	// Deploy Tasks
+	grunt.registerTask( 'ftp', ['ftp-deploy'] );
+
+	// Compress
+	grunt.registerTask( 'compress', [
+		'default',
+		'zip'
+	] );
+
+
+
+	// Short aliases
+	grunt.registerTask( 'w', ['watch'] );
+	grunt.registerTask( 'o', ['optimize'] );
+	grunt.registerTask( 'c', ['compress'] );
+};
