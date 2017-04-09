a8d4a559967188816fdf63a53756aef847cf82a7
Hugo Medeiros <hugo.hugomedeiros@gmail.com>
2017-04-08 23:11:10 -0300

    Support Fitvids

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,19 +6,6 @@ jQuery(document).ready(function($) {
 	    $('.collapse-side').toggleClass('in').toggleClass('hidden-xs').toggleClass('visible-xs');
 	});
 
-	$('input:-webkit-autofill').each(function(){
-        var text = $(this).val();
-        var name = $(this).attr('name');
-        $(this).after(this.outerHTML).remove();
-        $('input[name=' + name + ']').val(text);
-    });
-
-
-	if (navigator.userAgent.toLowerCase().indexOf('chrome') >= 0) {
-		$(window).load(function(){
-			$(':-webkit-autofill').each(function(){
-				$(this).after(this.outerHTML).remove();
-			});
-		});
-	}
+	// Target your .container, .wrapper, .post, etc.
+     $('iframe[src*="youtube"]').parent().fitVids();
 });
