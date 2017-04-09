786ef033e86a3f7ccb5888007282e75ead5b02ef
Hugo Medeiros <hugo.hugomedeiros@gmail.com>
2017-04-09 03:10:12 -0300

    front – Email collector

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,13 @@
 jQuery(document).ready(function($) {
+
+	// Open and CLose sidebar
+
 	$('[data-toggle=offcanvas]').click(function() {
-	    $('.row-offcanvas').toggleClass('active');
+		$('.row-offcanvas').toggleClass('active');
 		$('.sidebar-offcanvas').toggleClass('active');
 		$('.content').toggleClass('canvas-left');
-	    $('.collapse-side').toggleClass('in').toggleClass('hidden-xs').toggleClass('visible-xs');
 	});
 
-	// Target your .container, .wrapper, .post, etc.
-     $('iframe[src*="youtube"]').parent().fitVids();
-	 $('#results').fitVids();
+
+	
 });
