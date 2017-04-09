23b9bc5780f40a747ff1f56b3c20d8d115e3cddb
Hugo Medeiros <hugo.hugomedeiros@gmail.com>
2017-04-08 21:05:00 -0300

    Auth

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,4 +4,20 @@ jQuery(document).ready(function($) {
 		$('.sidebar-offcanvas').toggleClass('active');
 	    $('.collapse-side').toggleClass('in').toggleClass('hidden-xs').toggleClass('visible-xs');
 	});
+
+	$('input:-webkit-autofill').each(function(){
+        var text = $(this).val();
+        var name = $(this).attr('name');
+        $(this).after(this.outerHTML).remove();
+        $('input[name=' + name + ']').val(text);
+    });
+
+
+	if (navigator.userAgent.toLowerCase().indexOf('chrome') >= 0) {
+		$(window).load(function(){
+			$(':-webkit-autofill').each(function(){
+				$(this).after(this.outerHTML).remove();
+			});
+		});
+	}
 });
