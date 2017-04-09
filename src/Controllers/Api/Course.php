562131ebb6a08fb94ba84abe24a2cf952281e3f8
thiagoalencar <thiagofalencar@gmail.com>
2017-04-09 11:45:14 -0300

    - Adjust embedded code generate

--- src/Controllers/Api/Course.php
+++ src/Controllers/Api/Course.php
@@ -12,7 +12,7 @@ use Thinkific\Controllers\Controller;
 class Course extends Controller
 {
     public function getEmbedded($request, $response, $args){
-        $embedded =  sprintf("<iframe width=\"560\" height=\"315\" src=\"http://%s/%s/courses\" frameborder=\"0\" allowfullscreen></iframe>",$_SERVER["HTTP_HOST"], $args["id"]);
+        $embedded =  sprintf("<iframe width=\"560\" height=\"315\" src=\"http://%s/%s/courses/emb\" frameborder=\"0\" allowfullscreen></iframe>",$_SERVER["HTTP_HOST"], $args["id"]);
         echo $embedded;
     }
 }
